9c2283bd8e02da9b99a6c5d0a882507910dd7f96
Joaquin Martinez <joaco.martinez1480@gmail.com>
2026-03-12 17:01:47 -0300

    223

--- backend/src/schemas/orders.schemas.ts
+++ backend/src/schemas/orders.schemas.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
 
 export const createOrderSchema = z.object({
-  items: z.array(z.object({
-    productId: z.string().min(1),
-    quantity: z.number().int().min(1).default(1)
-  })).min(1)
+  country: z.string().optional(),
+
+  provider: z.enum(["MERCADOPAGO", "PAYPAL"]),
+
+  items: z
+    .array(
+      z.object({
+        productId: z.string().min(1),
+        quantity: z.number().int().min(1).default(1),
+      })
+    )
+    .min(1),
 });
